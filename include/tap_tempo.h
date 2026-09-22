439d1e5da7fc98f3b374fbef049d9e08f3cc08fe
tom-lyden <tomlyden.a@gmail.com>
2026-09-22 09:19:09 +0300

    Some polishes

--- include/tap_tempo.h
+++ include/tap_tempo.h
@@ -10,16 +10,16 @@
 #include <timer.h>
 #include <stdbool.h>
 
-#define MIN_TEMPO (20)
-#define MAX_TEMPO (240)
-#define DEFAULT_TEMPO (120)
+#define TAP_TEMPO_MIN_BPM (20)
+#define TAP_TEMPO_MAX_BPM (240)
+#define TAP_TEMPO_DEFAULT_BPM (120)
 
-#define MIN_DUTY_CYCLE (1)
-#define MAX_DUTY_CYCLE (99)
-#define DEFAULT_DUTY_CYCLE (50)
+#define TAP_TEMPO_MIN_DUTY_CYCLE (1)
+#define TAP_TEMPO_MAX_DUTY_CYCLE (99)
+#define TAP_TEMPO_DEFAULT_DUTY_CYCLE (50)
 
-#define INTERVAL_BUFFER_CAPACITY (2)
-#define MIN_INTERVALS_FOR_TEMPO (2)
+#define TAP_TEMPO_INTERVAL_BUFFER_CAPACITY (2)
+#define TAP_TEMPO_MIN_INTERVALS_FOR_TEMPO (2)
 
 typedef enum
 {
@@ -32,7 +32,7 @@ typedef void tap_tempo_set_indicator_t(tap_tempo_indicator_state_t);
 typedef struct
 {
 	get_ticks_t* get_ticks;
-	uint32_t tick_frequency;
+	uint32_t tick_frequency_hz;
 	tap_tempo_set_indicator_t* set_indicator;
 	uint8_t duty_cycle_percentage;
 	uint8_t initial_tempo;
@@ -44,17 +44,17 @@ typedef struct
 	uint32_t max_interval_ticks;
 	uint32_t reset_interval_ticks;
 	uint8_t duty_cycle_percentage;
-	
+
 	timer_t indicator_timer;
 	tap_tempo_set_indicator_t* set_indicator;
 	uint32_t high_duration_ticks;
 	uint32_t low_duration_ticks;
 	tap_tempo_indicator_state_t indicator_state;
-	
+
 	timer_t input_reset_timer;
 	get_ticks_t* get_ticks;
 	cqueue_t interval_queue;
-	uint32_t interval_buffer[INTERVAL_BUFFER_CAPACITY];
+	uint32_t interval_buffer[TAP_TEMPO_INTERVAL_BUFFER_CAPACITY];
 	uint64_t interval_tick_sum;
 	uint32_t previous_tap_ticks;
 	bool has_previous_tap;
--- src/tap_tempo.c
+++ src/tap_tempo.c
@@ -17,21 +17,21 @@ void TapTempo_Init(tap_tempo_t* tap_tempo, const tap_tempo_cfg_t* cfg)
 	tap_tempo->interval_tick_sum = 0;
 	tap_tempo->previous_tap_ticks = 0;
 	tap_tempo->has_previous_tap = false;
-	
-	CQueue_Init(&tap_tempo->interval_queue, tap_tempo->interval_buffer, INTERVAL_BUFFER_CAPACITY);
-	
-	tap_tempo->min_interval_ticks = tempo_period_ticks(cfg, MAX_TEMPO);
-	tap_tempo->max_interval_ticks = tempo_period_ticks(cfg, MIN_TEMPO);
-	tap_tempo->reset_interval_ticks = tempo_period_ticks(cfg, MIN_TEMPO) * INPUT_RESET_TO_MAX_TICKS_RATIO;
+
+	CQueue_Init(&tap_tempo->interval_queue, tap_tempo->interval_buffer, TAP_TEMPO_INTERVAL_BUFFER_CAPACITY);
+
+	tap_tempo->min_interval_ticks = tempo_period_ticks(cfg, TAP_TEMPO_MAX_BPM);
+	tap_tempo->max_interval_ticks = tempo_period_ticks(cfg, TAP_TEMPO_MIN_BPM);
+	tap_tempo->reset_interval_ticks = tempo_period_ticks(cfg, TAP_TEMPO_MIN_BPM) * INPUT_RESET_TO_MAX_TICKS_RATIO;
 	tap_tempo->duty_cycle_percentage = CLAMP(cfg->duty_cycle_percentage, MIN_DUTY_CYCLE, MAX_DUTY_CYCLE);
-	
+
 	uint8_t clamped_tempo = CLAMP(cfg->initial_tempo, MIN_TEMPO, MAX_TEMPO);
 	uint32_t period_ticks = tempo_period_ticks(cfg, clamped_tempo);
 	set_durations(tap_tempo, period_ticks);
 
 	tap_tempo->set_indicator = cfg->set_indicator;
 	tap_tempo->get_ticks = cfg->get_ticks;
-	
+
 	Timer_Init(&tap_tempo->indicator_timer, cfg->get_ticks);
 	Timer_Init(&tap_tempo->input_reset_timer, cfg->get_ticks);
 }
@@ -50,10 +50,10 @@ void TapTempo_Update(tap_tempo_t* tap_tempo)
 		tap_tempo->previous_tap_ticks = 0;
 		tap_tempo->interval_tick_sum = 0;
 		CQueue_Clear(&tap_tempo->interval_queue);
-		
+
 		Timer_Stop(&tap_tempo->input_reset_timer);
 	}
-	
+
 	if (Timer_GetState(&tap_tempo->indicator_timer) == TIMER_EXPIRED)
 	{
 		tap_tempo->indicator_state = tap_tempo->indicator_state == TAP_TEMPO_INDICATOR_STATE_HIGH
@@ -69,40 +69,40 @@ void TapTempo_ButtonPress(void* ctx)
 	tap_tempo_t* tap_tempo = (tap_tempo_t*)ctx;
 
 	Timer_Stop(&tap_tempo->input_reset_timer);
-	
+
 	uint32_t current_ticks = tap_tempo->get_ticks();
-	
+
 	if (!tap_tempo->has_previous_tap)
 	{
 		tap_tempo->previous_tap_ticks = current_ticks;
 		tap_tempo->has_previous_tap = true;
 		return;
 	}
-	
-	uint32_t new_interval =  current_ticks - tap_tempo->previous_tap_ticks;
+
+	uint32_t new_interval = current_ticks - tap_tempo->previous_tap_ticks;
 	tap_tempo->previous_tap_ticks = current_ticks;
-	 
+
 	if (CQueue_IsFull(&tap_tempo->interval_queue))
 	{
 		uint32_t oldest_interval = 0;
 		if (!CQueue_Pop(&tap_tempo->interval_queue, &oldest_interval))
 			return;
-		
+
 		tap_tempo->interval_tick_sum -= oldest_interval;
 	}
-	
+
 	if (!CQueue_Push(&tap_tempo->interval_queue, new_interval))
 		return;
-	
+
 	tap_tempo->interval_tick_sum += new_interval;
-	
+
 	uint8_t num_intervals = CQueue_Count(&tap_tempo->interval_queue);
-	if (num_intervals < MIN_INTERVALS_FOR_TEMPO)
+	if (num_intervals < TAP_TEMPO_MIN_INTERVALS_FOR_TEMPO)
 		return;
 
 	uint32_t period_ticks = tap_tempo->interval_tick_sum / num_intervals;
 	set_durations(tap_tempo, period_ticks);
-	
+
 	tap_tempo->indicator_state = TAP_TEMPO_INDICATOR_STATE_HIGH;
 	start_indicator_phase(tap_tempo);
 }
@@ -117,7 +117,7 @@ void TapTempo_ButtonRelease(void* ctx)
 static void set_durations(tap_tempo_t* tap_tempo, uint32_t period_ticks)
 {
 	uint32_t clamped_period_ticks = CLAMP(period_ticks, tap_tempo->min_interval_ticks, tap_tempo->max_interval_ticks);
-	
+
 	tap_tempo->high_duration_ticks = PERCENTAGE(clamped_period_ticks, tap_tempo->duty_cycle_percentage);
 	tap_tempo->low_duration_ticks = PERCENTAGE(clamped_period_ticks, 100 - tap_tempo->duty_cycle_percentage);
 }
@@ -135,5 +135,5 @@ static void start_indicator_phase(tap_tempo_t* tap_tempo)
 
 static inline uint32_t tempo_period_ticks(const tap_tempo_cfg_t* tap_tempo_cfg, uint32_t tempo)
 {
-	return 60 * tap_tempo_cfg->tick_frequency / tempo;
+	return 60 * tap_tempo_cfg->tick_frequency_hz / tempo;
 }
